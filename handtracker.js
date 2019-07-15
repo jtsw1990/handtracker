6a90685fe34c50478c868e71f9b1f9c382fce09c
jtsw1990 <jtsw1990@gmail.com>
2019-07-15 23:40:10 +1000

    Updated to print probability

--- handtracker.js
+++ handtracker.js
@@ -1,9 +1,9 @@
 const modelParams = {
-  flipHorizontal: true,   // flip e.g for video
-  imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
-  maxNumBoxes: 20,        // maximum number of boxes to detect
-  iouThreshold: 0.5,      // ioU threshold for non-max suppression
-  scoreThreshold: 0.79,    // confidence threshold for predictions.
+  flipHorizontal: true, // flip e.g for video
+  imageScaleFactor: 0.7, // reduce input image size for gains in speed.
+  maxNumBoxes: 5, // maximum number of boxes to detect
+  iouThreshold: 0.5, // ioU threshold for non-max suppression
+  scoreThreshold: 0.90, // confidence threshold for predictions.
 }
 
 navigator.getUserMedia = navigator.getUserMedia ||
@@ -35,10 +35,13 @@ handTrack.startVideo(video)
 function runDetection() {
   model.detect(video)
     .then(predictions => {
+      // document.getElementById("output").innerHTML = "SCANNING";
       console.log(predictions);
       // model.renderPredictions(predictions, canvas, context, video);
-      if(predictions.length > 0) {
+      if (predictions.length > 0) {
         audio.play();
+        console.log(predictions[0]["score"])
+        document.getElementById("output").innerHTML = "Probability output: " + predictions[0]["score"].toString();
       }
       requestAnimationFrame(runDetection);
     });
